79781789c17af878e8eddaac63d1dbc71a499b0c
zhuocheng.ly <zhuocheng.ly@alibaba-inc.com>
2018-06-05 16:00:12 +0800

    commit mochawesome-zc code

---
+++ test/basic/test.js
@@ -0,0 +1,43 @@
+var should = require('should');
+var fs = require('fs');
+
+describe('sample/测试_spec : chrome', function () {
+  it('url: http://www.baidu.com', function(){
+      aaa
+  });
+  
+  it('waitBody: ', function(){
+      bbb
+  });
+  
+  it('click: wd ( #kw, 183, 14, 0 )', function(done){
+      setTimeout(done, 200);
+  });
+  
+  it('sendKeys: mp3{ENTER}', function(){
+      
+  });
+
+  it('imgdiff: u1 ( #u1 )', function(){
+      
+  });
+});
+
+describe('sample/测试_spec : ie 11', function () {
+  it('url: http://www.baidu.com', function(done){
+      setTimeout(done, 800);
+  });
+  
+  it('waitBody: ', function(){
+      
+  });
+  
+  it('click: wd ( #kw, 183, 14, 0 )', function(){
+      
+  });
+  
+  it('sendKeys: mp3{ENTER}', function(){
+      
+  });
+
+});
